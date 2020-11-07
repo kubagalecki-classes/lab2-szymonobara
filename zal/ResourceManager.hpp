3a9bd8b54c656611392e8980514b464e751c374a
szymonobara <Szymon2904@gmail.com>
2020-11-07 18:14:02 +0000

    solved

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -7,16 +7,20 @@ class ResourceManager
     public:
     ResourceManager(){}
 
-    ResourceManager(const ResourceManager& rm): get(){rm.get()} {}
-    
+    ResourceManager(const ResourceManager& rm) 
+    {
+      get()=rm.get();
+    }
     ResourceManager& operator=(ResourceManager& rm)
     {
       get()=rm.get();
       return *this;
     }
 
-    ResourceManager(ResourceManager&& rm): get(){std::move(rm.get())} {}
-    
+    ResourceManager(ResourceManager&& rm)
+    {
+      get()=std::move(rm.get());
+    }
     ResourceManager& operator=(ResourceManager&& rm)
     {
         get()=std::move(rm.get());
